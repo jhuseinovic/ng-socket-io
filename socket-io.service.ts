7cb607409524f0e1b4aec67b26b30210e9f4ef46
John Huseinovic <john@huseinovic.net>
2022-11-02 15:18:17 +0000

    Update socket-io.service.ts

--- socket-io.service.ts
+++ socket-io.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, EventEmitter, Inject } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import {  Inject } from '@angular/core';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/share'; 
 
-import * as io from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 import { SocketIoConfig } from './socketIoConfig';
 import { SOCKET_CONFIG_TOKEN } from './socket-io.module';
